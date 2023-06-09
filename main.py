c3dbfff9c2ec847cbda9b328bea652c688f1c4ed
Morvarid Rahbar <morvarid.rahbar@gmail.com>
2023-06-10 00:46:28 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,6 @@
-# Program to add two matrices using nested loop
 
-X = [[12,7,3],
-    [4 ,5,6],
-    [7 ,8,9]]
-
-Y = [[5,8,1],
-    [6,7,3],
-    [4,5,9]]
-
-result = [[0,0,0],
-         [0,0,0],
-         [0,0,0]]
-
-# iterate through rows
-for i in range(len(X)):
-   # iterate through columns
-   for j in range(len(X[0])):
-       result[i][j] = X[i][j] + Y[i][j]
-
-for r in result:
-   print(r)
+num = int(input("Enter a number: "))
+if (num % 2) == 0:
+   print("{0} is Even".format(num))
+else:
+   print("{0} is Odd".format(num))
