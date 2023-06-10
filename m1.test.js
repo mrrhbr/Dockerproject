a620023d099cfb25a9fa9b95ffb5fb3b7b0b498a
Morvarid Rahbar <morvarid.rahbar@gmail.com>
2023-06-10 15:23:51 +0330

    1000th

---
+++ m1.test.js
@@ -0,0 +1,42 @@
+const { add, subtract, divide, multiply } = require("./math");
+describe("Math. js tests", () => {
+  it("should test add ",()=>{
+    expect (add (2, 2)). toBe (4) ;
+  });
+  it("should test subtract",()=>{
+    expect (subtract (2, 2)). toBe (0) ;
+  });
+
+  it("should test devide",()=>{
+    expect (divide (2, 2)). toBe (1) ;
+  });
+  it("should test mulitply",()=>{
+    expect (multiply (2, 2)). toBe (4) ;
+  });
+  it("should test add ",()=>{
+    expect (add (15, 3)). toBe (18) ;
+  });
+  it("should test subtract",()=>{
+    expect (subtract (15, 3)). toBe (12) ;
+  });
+
+  it("should test devide",()=>{
+    expect (divide (15, 3)). toBe (5) ;
+  });
+  it("should test mulitply",()=>{
+    expect (multiply (15, 3)). toBe (45) ;
+  });
+  it("should test add ",()=>{
+    expect (add (20, 4)). toBe (24) ;
+  });
+  it("should test subtract",()=>{
+    expect (subtract (20, 4)). toBe (16) ;
+  });
+
+  it("should test devide",()=>{
+    expect (divide (20, 4)). toBe (5) ;
+  });
+  it("should test mulitply",()=>{
+    expect (multiply (20, 4)). toBe (60) ;
+  });
+});
